2ab909622ccd3decb374c73d9a648b627a7d3c4f
Elite_tea <andreewkirill64@yandex.ru>
2025-05-23 17:04:58 +0400

    feat: Заготовочка

---
+++ src/main/java/ru/ai/autoAssign/AutoAssignAiApplication.java
@@ -0,0 +1,13 @@
+package ru.ai.autoAssign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoAssignAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoAssignAiApplication.class, args);
+	}
+
+}
